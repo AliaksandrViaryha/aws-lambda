ea8dd79b7e5d92446cbd028404537fe66a07f080
Aliaksandr Viaryha <aliaksandr.viaryha@gmail.com>
2023-06-02 00:08:23 +0300

    Created logic for getting post by id

--- aws-node-lambda/index.js
+++ aws-node-lambda/index.js
@@ -11,3 +11,16 @@ module.exports.handler = async (event) => {
     statusCode: 200,
   };
 };
+
+module.exports.getOne = async (event) => {
+  const id = event.pathParameters.id;
+  const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const post = response.data;
+
+  return {
+    body: JSON.stringify({
+        body: post
+    }),
+    statusCode: 200,
+  };
+};
