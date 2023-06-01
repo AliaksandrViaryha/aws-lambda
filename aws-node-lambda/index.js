730f85852d8f1a24924cab7bc59dc2b969b2ec47
Aliaksandr Viaryha <aliaksandr.viaryha@gmail.com>
2023-06-02 00:05:29 +0300

    Installed axios. Updated logic for lambda and serverless.yml

--- aws-node-lambda/index.js
+++ aws-node-lambda/index.js
@@ -1,13 +1,13 @@
+const axios = require('axios');
+
 module.exports.handler = async (event) => {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  const posts = response.data;
+
   return {
+    body: JSON.stringify({
+        body: posts
+    }),
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
   };
 };
